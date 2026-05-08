9a1f17cfc1963295090645d8e0419735ec14c2eb
Daniel <danielbnhz@gmail.com>
2026-05-08 14:42:12 -0700

    adds basic banner image to front-page

---
+++ front-page.php
@@ -0,0 +1,17 @@
+<?php get_header(); ?>
+
+<main class="site-main">
+    <header class="site-header">
+        <h1 class="site-title">NTH_TECH</h1>
+    </header>
+
+
+
+    <img 
+        src="<?php echo get_template_directory_uri(); ?>/assets/images/blue_session.webp" 
+        alt="nth_tech FL Studio session"
+        class="session-image"
+    >
+</main>
+
+<?php get_footer(); ?>
